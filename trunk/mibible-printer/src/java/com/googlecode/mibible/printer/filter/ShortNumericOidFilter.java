7cd645aafcd09fa145425c68abc28ea75deacf72
tknstyk <tknstyk@a253fe74-9d24-11dd-bde8-492b52a95b86>
2008-10-22 22:25:36 +0000

    Issues #8 対応

---
+++ trunk/mibible-printer/src/java/com/googlecode/mibible/printer/filter/ShortNumericOidFilter.java
@@ -0,0 +1,33 @@
+package com.googlecode.mibible.printer.filter;
+
+import net.percederberg.mibble.value.ObjectIdentifierValue;
+
+/**
+ * OIDをshort textual formに整形するクラス。</br>
+ * 整形例</br>
+ * OIDにIF-MIBのifOperStatusを指定した場合、"%num"という文字列を
+ * 以下のフォーマットに整形する。</br>
+ * 8
+ * 
+ * @author snuffkin
+ * @since 0.2.0
+ */
+public class ShortNumericOidFilter extends PrintFilter
+{
+    /**
+     * コンストラクタ。
+     */
+    public ShortNumericOidFilter()
+    {
+        super("%num");
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getPrintString(ObjectIdentifierValue oid)
+    {
+        return String.valueOf(oid.getValue());
+    }
+}
