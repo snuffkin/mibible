406ea9ba991ff88fd75cd3f3c0083e47e16bab8b
tknstyk <tknstyk@a253fe74-9d24-11dd-bde8-492b52a95b86>
2008-10-25 13:50:48 +0000

    mibible-printer-0.2.0

---
+++ tags/mibible-printer-0.2.0/src/java/com/googlecode/mibible/printer/filter/LongNumericOidFilter.java
@@ -0,0 +1,50 @@
+package com.googlecode.mibible.printer.filter;
+
+import net.percederberg.mibble.value.ObjectIdentifierValue;
+
+/**
+ * OIDをlong numeric formに整形するクラス。</br>
+ * 整形例</br>
+ * OIDにIF-MIBのifOperStatusを指定した場合、"%Num"という文字列を
+ * 以下のフォーマットに整形する。</br>
+ * 1.3.6.1.2.1.2.2.1.8
+ * 
+ * @author snuffkin
+ * @since 0.2.0
+ */
+public class LongNumericOidFilter extends PrintFilter
+{
+    /**
+     * コンストラクタ。
+     */
+    public LongNumericOidFilter()
+    {
+        super("%Num");
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getPrintString(ObjectIdentifierValue oid)
+    {
+        ObjectIdentifierValue tmpOid = oid;
+        String ret = "";
+        
+        // 末端からから上位の順に、numeric formを作成する
+        while (tmpOid != null)
+        {
+            if (ret.isEmpty())
+            {
+                ret = String.valueOf(tmpOid.getValue());
+            }
+            else
+            {
+                ret = tmpOid.getValue() + "." + ret;
+            }
+            tmpOid = tmpOid.getParent();
+        }
+        
+        return ret;
+    }
+}
