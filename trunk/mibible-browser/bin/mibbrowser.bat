2f3aa2f95bf3df93d3bdb18eb67a18e67904d5b3
tknstyk <tknstyk@a253fe74-9d24-11dd-bde8-492b52a95b86>
2008-11-24 14:32:44 +0000

    0.3.0beta用の環境修正

--- trunk/mibible-browser/bin/mibbrowser.bat
+++ trunk/mibible-browser/bin/mibbrowser.bat
@@ -7,7 +7,7 @@ REM set MIBIBLE_HOME variable
 set MIBIBLE_HOME=..
 
 REM set CLASSPATH variable
-set CLASSPATH=%MIBIBLE_HOME%\lib\mibible-browser-0.2.0.jar;%MIBIBLE_HOME%\lib\mibble-parser-2.8.jar;%MIBIBLE_HOME%\lib\mibble-mibs-2.8.jar;%MIBIBLE_HOME%\lib\grammatica-bin-1.4.jar;%MIBIBLE_HOME%\lib\snmp4_13.jar;%MIBIBLE_HOME%\lib\h2-1.1.103.jar
+set CLASSPATH=%MIBIBLE_HOME%\lib\mibible-browser-0.3.0beta.jar;%MIBIBLE_HOME%\lib\mibble-parser-2.8.jar;%MIBIBLE_HOME%\lib\mibble-mibs-2.8.jar;%MIBIBLE_HOME%\lib\grammatica-bin-1.4.jar;%MIBIBLE_HOME%\lib\snmp4_13.jar;%MIBIBLE_HOME%\lib\h2-1.1.103.jar
 
 REM run the mibible browser
 java com.googlecode.mibible.browser.MibBrowser
