415f5f359769520560d862fb692033b79a77ffbd
tknstyk <tknstyk@a253fe74-9d24-11dd-bde8-492b52a95b86>
2008-11-24 15:09:40 +0000

    0.3.0beta

---
+++ tags/mibible-printer-0.3.0beta/bin/mibprinter.bat
@@ -0,0 +1,13 @@
+@echo off
+REM
+REM browser.bat: runs the mibible browser
+REM
+
+REM set MIBIBLE_HOME variable
+set MIBIBLE_HOME=..
+
+REM set CLASSPATH variable
+set CLASSPATH=%MIBIBLE_HOME%\lib\mibible-printer-0.3.0beta.jar;%MIBIBLE_HOME%\lib\mibble-parser-2.8.jar;%MIBIBLE_HOME%\lib\mibble-mibs-2.8.jar;%MIBIBLE_HOME%\lib\grammatica-bin-1.4.jar;%MIBIBLE_HOME%\lib\snmp4_13.jar
+
+REM run the mibible browser
+java com.googlecode.mibible.printer.MibPrinter %1 %2 %3 %4 %5 %6 %7 %8 %9
