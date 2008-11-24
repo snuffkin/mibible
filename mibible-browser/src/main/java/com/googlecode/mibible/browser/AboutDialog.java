d2cc6d8cae57e4233b810ca5b1bf078b6e4fbec0
tknstyk <tknstyk@a253fe74-9d24-11dd-bde8-492b52a95b86>
2008-11-24 15:18:55 +0000

    0.3.0beta

--- mibible-browser/src/main/java/com/googlecode/mibible/browser/AboutDialog.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.mibible.browser;
-
-import javax.swing.JDialog;
-
-public class AboutDialog extends JDialog {
-
-}
--- mibible-browser/src/main/java/com/googlecode/mibible/browser/LicenseDialog.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.mibible.browser;
-
-import javax.swing.JDialog;
-
-public class LicenseDialog extends JDialog {
-
-}
